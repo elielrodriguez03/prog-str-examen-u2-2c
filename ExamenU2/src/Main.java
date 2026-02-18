77db4c6e1ff0616f431b4dfdf22e3d878ba2aa11
MISA-BB <20253ds083@utez.edu.mx>
2026-02-18 09:26:44 -0600

    kjhgfjkgh

--- ExamenU2/src/Main.java
+++ ExamenU2/src/Main.java
@@ -52,6 +52,7 @@ public class Main {
         System.out.println("Denegados: " + totalDenegados);
 
 
+
         double porcentaje=(cienPorcentaje /totalRegistros)*totalPermitidos; //Falta hacer el calculo de porcentaje
         System.out.println("Porcentaje permitidos: %"+ porcentaje );
     }
