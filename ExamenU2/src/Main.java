1e242e7cfd8a640dcd2d7c004eaa790df0204951
VelazquezMartinezSebastian <20253rd029@utez.edu.mx>
2026-02-18 09:06:45 -0600

    Examen Practico U2 Melisa Sanchez

--- ExamenU2/src/Main.java
+++ ExamenU2/src/Main.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int totalRegistros = 0;
+        int totalPermitidos = 0;
+        int totalDenegados = 0;
+        int cien = 100;
 
 
         while (true) {
@@ -12,7 +16,12 @@ public class Main {
             if (id.equalsIgnoreCase("FIN")) break;
 
             System.out.print("Hora (0..23): ");
+            if (!sc.hasNextInt()){
+                System.out.println("Hora inválida. Fin del programa.");
+                return;
+            }
             int hora = sc.nextInt();
+
 
             // Si hora fuera 0..23 -> terminar main
             if (hora < 0 || hora > 23) {
@@ -42,7 +51,7 @@ public class Main {
         System.out.println("Permitidos: " + totalPermitidos);
         System.out.println("Denegados: " + totalDenegados);
 
-        double porcentaje=0; //Falta hacer el calculo de porcentaje
-        System.out.println("Porcentaje permitidos: %"+ porcentaje);
+        double porcentaje=(cien /totalRegistros)*totalPermitidos; //Falta hacer el calculo de porcentaje
+        System.out.println("Porcentaje permitidos: %"+ porcentaje );
     }
 }
